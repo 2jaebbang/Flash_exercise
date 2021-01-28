b8b59a3b27cf2e214e65ba95c625520434790e91
jaeyoung Lee <2jaebbang@gmail.com>
2021-01-28 11:09:22 +0900

    test

--- Flash_exercise/ViewController.swift
+++ Flash_exercise/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var flashView: UIImageView!
+    
+    var soundPlayer: AVAudioPlayer?
+    var isOn = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prepareSound()
+    }
+    
+    func toggleFlash() {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+
+        do {
+            try device.lockForConfiguration()
+
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func prepareSound(){
+        let path = Bundle.main.path(forResource: "switch.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do{
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.prepareToPlay()
+        } catch {
+            
+        }
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        toggleFlash()
+        soundPlayer?.play()
+        isOn = !isOn
+        
+        if isOn == true{
+            button.setImage(#imageLiteral(resourceName: "onSwitch"), for: .normal)
+            flashView.image = #imageLiteral(resourceName: "onBG")
+        } else {
+            button.setImage(#imageLiteral(resourceName: "offSwitch"), for: .normal)
+            flashView.image = #imageLiteral(resourceName: "offBG")
+        }
+      
+    }
+    
 }
 
